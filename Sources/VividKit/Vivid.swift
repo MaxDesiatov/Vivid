430cac3a8ab28cbcd9e8a187c84b8789c7c88330
Max Desiatov <max@desiatov.com>
2019-01-20 10:56:14 +0000

    Add Xcode project and CodeMirror without modes

--- Sources/VividKit/Vivid.swift
+++ Sources/VividKit/Vivid.swift
@@ -1,13 +1,67 @@
 import Foundation
 import JavaScriptCore
 
+let highlighterSource = """
+const CodeMirror = exports;
+function highlightCode(language, value) {
+  const elements = [];
+  let lastStyle = null;
+  let tokenBuf = "";
+  const pushElement = (token, style) => {
+    elements.push(
+      `<span${style ? ` class="cm-${style}"` : ""}>${token}</span>`
+    );
+  };
+  CodeMirror.runMode(value, language, (token, style) => {
+    if (lastStyle === style) {
+      tokenBuf += token;
+      lastStyle = style;
+    } else {
+      if (tokenBuf) {
+        pushElement(tokenBuf, lastStyle);
+      }
+      tokenBuf = token;
+      lastStyle = style;
+    }
+  });
+  pushElement(tokenBuf, lastStyle);
+
+  return elements.join("");
+};
+"""
+
+extension JSContext {
+  func setupCommonJS() {
+    setObject([:], forKeyedSubscript: "exports" as NSString)
+    let resolve: @convention(block) (String) -> () = { _ in }
+    setObject([
+      "cache": [:] as NSDictionary,
+      "resolve": JSValue(object: resolve, in: self)
+    ] as [NSString: AnyHashable], forKeyedSubscript: "require" as NSString)
+  }
+}
+
 public struct Vivid {
   let context = JSContext()!
+  let highlighter: JSValue
 
   public init(nodePath: String) throws {
+    context.setupCommonJS()
     let scriptPath = "\(nodePath)/codemirror/addon/runmode/runmode.node.js"
-    context.evaluateScript(try String(contentsOf: URL(fileURLWithPath: scriptPath)))
+    let root = URL(string: "file:///")!
+    let scriptURL = URL(
+      fileURLWithPath: scriptPath,
+      isDirectory: false,
+      relativeTo: root
+    )
+    context.evaluateScript(try String(contentsOf: scriptURL))
+    context.evaluateScript(highlighterSource)
+    highlighter = context.evaluateScript("highlightCode")
   }
 
-  func highlight(input: String) {}
+  public func highlight(input: String) -> String? {
+    let result = highlighter.call(withArguments: ["swift", input])?.toString()
+    print(context.exception)
+    return result
+  }
 }
--- Sources/vivid/main.swift
+++ Sources/vivid/main.swift
@@ -1,3 +1,13 @@
 import VividKit
 
-let v = try? Vivid(nodePath: "\(#file)/../../node_modules")
+let dir = #file.split(separator: "/").dropLast().joined(separator: "/")
+let v: Vivid
+do {
+  v = try Vivid(nodePath: "\(dir)/../../node_modules")
+} catch {
+  fatalError(String(describing: error))
+}
+
+print(v.highlight(input: """
+  public static func test() { print("blah")}
+"""))
